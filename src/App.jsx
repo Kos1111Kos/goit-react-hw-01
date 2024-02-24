f3aaa708fede833a9a9487717165df06588fe264
Kos1111Kos <konstantin.smirnov.2011@gmail.com>
2024-02-24 14:21:14 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,6 @@
 import "./App.css";
-import Profile from "./components/Profile/Profile";
-
-const userData = {
-  username: "Jacques Gluke",
-  tag: "@jgluke",
-  location: "Ocho Rios, Jamaica",
-  avatar: "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
-  stats: {
-    followers: 5603,
-    views: 4827,
-    likes: 1308,
-  },
-};
-
+import Profile from "./Profile/ProfileCard/Profile";
+import userData from "./userData.json";
 function App() {
   return (
     <div>
@@ -26,31 +14,5 @@ function App() {
     </div>
   );
 }
-
-// function App() {
-//   return (
-//     <div>
-//       {productDAta.map((item) => {
-//         return (
-//           <Profile
-//             key={item.id}
-//             productName={item.productName}
-//             price={item.price}
-//             hasDiscount={item.hasDiscount}
-//             description={item.description}
-//             img={item.img}
-//           />
-//         );
-//       })}
-//       {/* <ProductCard
-//         productName="jeep black"
-//         price={100}
-//         hasDiscount={true}
-//         description="ffppksfmkkk"
-//         img="https://img.glavnoe.in.ua/uploads/2024/02/02082201/7594ce72-eed721d62acaf70de036fae71fbe88d2.jpg"
-//       /> */}
-//     </div>
-//   );
-// }
 
 export default App;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,23 +1,24 @@
-import "./ProfileStyles.css";
+import css from "./ProfileStyle.module.css";
+
 const Profile = ({ image, name, tag, location, stats }) => {
   return (
-    <div className="card">
-      <div className="cardCont">
-        <img className="cardImg" src={image} width={260} alt="" />
-        <p className="cardName">{name}</p>
-        <p className="cardTag">{tag}</p>
-        <p className="cardLocation">{location}</p>
+    <div className={css.card}>
+      <div className={css.cardCont}>
+        <img className={css.cardImg} src={image} width={260} alt="" />
+        <p className={css.cardName}>{name}</p>
+        <p className={css.cardTag}>{tag}</p>
+        <p className={css.cardLocation}>{location}</p>
       </div>
       {/* об'єкт з інформацією про активності */}
-      <ul className="cardItems">
-        <li className="cardList">
-          Followers: <span className="cardListText">{stats.followers}</span>
+      <ul className={css.cardItems}>
+        <li className={css.cardList}>
+          Followers: <span className={css.cardListText}>{stats.followers}</span>
         </li>
-        <li className="cardList">
-          Views: <span className="cardListText">{stats.followers}</span>
+        <li className={css.cardList}>
+          Views: <span className={css.cardListText}>{stats.followers}</span>
         </li>
-        <li className="cardList">
-          Likes: <span className="cardListText">{stats.followers}</span>
+        <li className={css.cardList}>
+          Likes: <span className={css.cardListText}>{stats.followers}</span>
         </li>
       </ul>
     </div>
